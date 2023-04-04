3f398a447b4abbee5cc37e3724361f010439cb83
maker-pro <85552249+maker-pro@users.noreply.github.com>
2023-04-04 11:31:08 +0800

    2023-04-04 BaseModel 完善

--- model/BaseModel.php
+++ model/BaseModel.php
@@ -14,8 +14,12 @@ use \common\Result;
 class BaseModel
 {
     protected $table = null;
-    protected $where_array = [];
     protected $mysql_object = null;
+    protected $where_array = [];
+    protected $group_by_field = null;
+    protected $order_by = [];
+    protected $offset = null;
+    protected $limit = null;
 
     public function __construct()
     {
@@ -38,8 +42,27 @@ class BaseModel
         $sql .= 'from `' . $this->table . '` ';
 
         if ($this->where_array) {
-            $sql .= 'where ' . implode(' and ', $this->where_array);
+            $sql .= 'where ' . implode(' and ', $this->where_array) . ' ';
         }
+        if ($this->order_by) {
+            $order_by_arr = [];
+            foreach ($this->order_by as $field => $order_by_type) {
+                $order_by_arr[] = $field . ' ' . $order_by_type;
+            }
+            $sql .= 'order by ' . implode(', ', $order_by_arr) . ' ';
+        }
+        if (!is_null($this->limit)) {
+            $limit_string = 'limit ';
+            if (!is_null($this->offset)) {
+                $limit_string .= $this->offset . ', ';
+            }
+            $limit_string .= $this->limit;
+            $sql .= $limit_string . ' ';
+        }
+        if (!is_null($this->group_by_field)) {
+            $sql .= 'group by ' . $this->group_by_field;
+        }
+
         if ($index_field) {
             $result = $this->mysql_object->getRows($sql, $index_field);
         } else {
@@ -62,5 +85,19 @@ class BaseModel
         return $this;
     }
 
+    protected function orderBy($order_by = []) {
+        $this->order_by = $order_by;
+        return $this;
+    }
 
+    protected function groupBy($group_by_field = '') {
+        $this->group_by_field = $group_by_field;
+        return $this;
+    }
+
+    protected function limit($limit = 10, $offset = null) {
+        $this->limit = $limit;
+        $this->offset = $offset;
+        return $this;
+    }
 }
